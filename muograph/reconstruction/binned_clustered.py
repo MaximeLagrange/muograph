2643ce2b6c057f7091dad7b6c772b035d715349f
Jimena Gómez Fernández <uovi1239@mlui01.ific.uv.es>
2025-03-17 12:58:46 +0100

    Modified POCA.__init__ in line 59 to include argument "filename"

--- muograph/reconstruction/binned_clustered.py
+++ muograph/reconstruction/binned_clustered.py
@@ -56,7 +56,7 @@ class BCA(POCA, AbsVoxelInferer):
             and `hit_per_voxel` attributes in a hdf5 file.
         """
         AbsVoxelInferer.__init__(self, voi=voi, tracking=tracking)
-        POCA.__init__(self, tracking=tracking, voi=voi, output_dir=output_dir)
+        POCA.__init__(self, tracking=tracking, voi=voi, output_dir=output_dir, filename = filename)
 
         self.bca_indices: Tensor = deepcopy(self.poca_indices)
         self.bca_poca_points: Tensor = deepcopy(self.poca_points)
